fef7ffb42dc92afbcdab6cffef62603b03e60371
Patryk Rudzinski <patrykrudzinski@gmail.com>
2019-10-12 11:04:05 +0200

    fix dependencies

--- src/structure/views/Login/Login.jsx
+++ src/structure/views/Login/Login.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import { withApollo } from 'react-apollo';
 import Main from '../../../containers/Main';
-import Logo from '../../../components/Logo';
-import getInitialData from "../../../logic/hooks/getInitialData";
 
 const Login = () => {
-
-    const [data, loading] = getInitialData(
-      `{
-            rates(currency: "USD") {
-              currency
-            }
-          }`
-    );
-
     return (
       <Main>
-        {loading ? <h1>LOADING</h1> : <h1>{data.rates && data.rates[1].currency}</h1>}
-        <Logo/>
       </Main>
     )
 };
