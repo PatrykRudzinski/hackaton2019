81a7cc5e85b3922d8932a7f2dd8acea46a27e6d4
Alan <rudzinski.a@gmail.com>
2019-10-12 18:42:35 +0200

    customize ant fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import  { BreakpointProvider } from 'react-socks';
 import client from './config/apollo/client';
 import theme from './config/styles/theme';
 import Router from './structure/Router';
-import 'antd/dist/antd.css';
 
 const App = () => (
     <ApolloProvider client={client}>
--- src/containers/Layout/LayoutGeneral/_components/Navigation/Navigation.jsx
+++ src/containers/Layout/LayoutGeneral/_components/Navigation/Navigation.jsx
@@ -1,28 +1,57 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom'
-import {Menu, Icon} from 'antd';
+import {Menu, Icon, Button} from 'antd';
 import {withRouter} from 'react-router';
+import styled from 'styled-components';
+
+// const styledItem = styled(Item)`
+
+// `
 
 const {Item} = Menu;
 
 const Navigation = ({horizontal}) => {
 
   return (
-    <Menu mode={horizontal ? 'horizontal' : 'inline'}>
-      <Item key="1">
-        <NavLink to='/'>
-          <Icon type="ordered-list"/>
-          <span>Dashboard</span>
-        </NavLink>
-      </Item>
-      <Item key="2">
-        <NavLink to='/'>
-          <Icon type="ordered-list"/>
-          <span>Ustawienia</span>
-        </NavLink>
-      </Item>
-    </Menu>
+      <Menu mode={horizontal ? 'horizontal' : 'inline'}>
+          <Item key="1">
+            <NavLink to='/'>
+              <Icon type="home" />
+              <span>Potrzebuje pomocy<Button type="primary">dsad</Button></span>
+            </NavLink>
+          </Item>
+          <Item key="2">
+            <NavLink to='/'>
+              <Icon type="heart" />
+              <span>Chce pomoc</span>
+            </NavLink>
+          </Item>
+          <Item key="3">
+            <NavLink to='/'>
+              <Icon type="setting" />
+              <span>Ustawienia</span>
+            </NavLink>
+          </Item>
+      </Menu>
   );
 };
 
 export default withRouter(Navigation);
+
+{/* <SubMenu
+          title={
+            <span className="submenu-title-wrapper">
+              <Icon type="setting" />
+              Navigation Three - Submenu
+            </span>
+          }
+        >
+          <Menu.ItemGroup title="Item 1">
+            <Menu.Item key="setting:1">Option 1</Menu.Item>
+            <Menu.Item key="setting:2">Option 2</Menu.Item>
+          </Menu.ItemGroup>
+          <Menu.ItemGroup title="Item 2">
+            <Menu.Item key="setting:3">Option 3</Menu.Item>
+            <Menu.Item key="setting:4">Option 4</Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu> */}
