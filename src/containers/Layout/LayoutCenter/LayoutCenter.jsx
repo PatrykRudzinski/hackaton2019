8a4a4e3c95a6ceed0acbb1ff17c54ca372fc6095
Patryk Rudzinski <patrykrudzinski@gmail.com>
2019-10-12 15:12:19 +0200

    layout center

--- src/containers/Layout/LayoutCenter/LayoutCenter.jsx
+++ src/containers/Layout/LayoutCenter/LayoutCenter.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-  
-const LayoutCenter = () => {
-  return (
-    <>
-      LayoutCenter
-    </>
-)};
+import styled from 'styled-components';
+
+const StyledLayoutCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`;
+
+const LayoutCenter = ({children}) => (
+    <StyledLayoutCenter>
+        {children}
+    </StyledLayoutCenter>
+);
 
 export default LayoutCenter;
