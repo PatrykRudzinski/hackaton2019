fa4039008b3ecf58fd3451386c91f35a55631316
Alan <rudzinski.a@gmail.com>
2019-10-12 17:09:36 +0200

    register google facebook

--- src/structure/views/AddNotice/AddNotice.jsx
+++ src/structure/views/AddNotice/AddNotice.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import LayoutCenter from '../../../../src/containers/Layout/LayoutCenter/LayoutCenter'
   
 const AddNotice = () => {
   return (
-    <>
+    <LayoutCenter>
       AddNotice
-    </>
+    </LayoutCenter>
 )};
 
 export default AddNotice;
