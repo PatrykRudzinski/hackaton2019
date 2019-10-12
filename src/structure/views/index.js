56827072d22ad00f49b007958db908a6102c9248
Patryk Rudzinski <patrykrudzinski@gmail.com>
2019-10-12 15:00:30 +0200

    resolve conflicts

--- src/structure/views/index.js
+++ src/structure/views/index.js
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+import AddNotice from './AddNotice';
+import Dashboard from './Dashboard';
 import Error from './Error';
 import Login from './Login';
 import Register from './Register';
@@ -6,23 +7,11 @@ import UpdateProfileLocation from './UpdateProfileLocation';
 import UpdateProfileRole from './UpdateProfileRole';
 
 export {
+	AddNotice,
+	Dashboard,
 	Error,
 	Login,
 	Register,
 	UpdateProfileLocation,
 	UpdateProfileRole,
-=======
-import AddNotice from './AddNotice';
-import Dashboard from './Dashboard';
-import Error from './Error';
-import Login from './Login';
-import Products from './Products';
-
-export {
-	AddNotice,
-	Dashboard,
-	Error,
-	Login,
-	Products,
->>>>>>> fcc88bde7103c3e18d2c51e07e79153d69fc2277
 };
