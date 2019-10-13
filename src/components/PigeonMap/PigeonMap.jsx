fdd9a375566c4e1ba39cc864b6ddcfa4273ded85
michalsut <sut.michal@gmail.com>
2019-10-13 15:05:11 +0200

    improved maps

--- src/components/PigeonMap/PigeonMap.jsx
+++ src/components/PigeonMap/PigeonMap.jsx
@@ -4,28 +4,9 @@ import React from 'react';
 import Map from 'pigeon-maps';
 import Marker from 'pigeon-marker';
 
-
 import './PigeonMaps.styl';
 
-// export const markers = [
-//     {
-//         name: 'Kottbusser Tor',
-//         latlng: [52.499040, 13.418392]
-//     }, {
-//         name: 'Görlitzer Park',
-//         latlng: [52.496912, 13.436738]
-//     }, {
-//         name: 'webkid',
-//         latlng: [52.501106, 13.422061]
-//     }
-// ];
-//
-// export const mapConfig = {
-//     center: [52.499219, 13.425416],
-//     zoom: 15
-// };
-
-const getProvider = (x, y, z) => `http://a.tile.openstreetmap.fr/hot/${z}/${x}/${y}.png`;
+const getProvider = (x, y, z) => `https://a.tile.openstreetmap.org/${z}/${x}/${y}.png`;
 
 
 const PigeonMap = ({notice}) => {
@@ -44,7 +25,7 @@ const PigeonMap = ({notice}) => {
                 width={500}
                 height={500}
                 defaultCenter={notice.address.geometry.coordinates.reverse()}
-                defaultZoom={10}
+                defaultZoom={13}
                 provider={getProvider}
             >
                 {Markers}
